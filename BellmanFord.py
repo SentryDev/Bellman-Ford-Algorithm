3cb76cd7a10a1a393a86769f237e287a72512c42
SentryDev <FlamencoDeveloper@gmail.com>
2021-03-18 00:38:21 +0000

    1st Initialisation, Defined Link Data structure, +Tested

---
+++ BellmanFord.py
@@ -0,0 +1,24 @@
+# Bellmand Ford Algorithm calculates the shortest path form a node to another Node
+
+# Algorithm overview
+# Computation data requires leading, trailing node and connecting Link
+# Operations include access of leading, trailing and connecting link
+# Addition arithmetic, comparison, and write to memory
+
+
+# 1) Modeling the graphical data structure
+# We define the graphical links using a class Data structure
+# Attach the leading node value, Link value and Trailing node address as Attributes
+
+class Link:
+    
+    def __init__(self, leading_value, link_value, trailing_address):
+        #Initialize the initial values of the graphical structure
+        self.leading_value = leading_value
+        self.link_value = link_value
+        self.trailing_address = trailing_address
+
+
+link_1 = Link(6, 10, 2)
+print("Leading value:" + str(link_1.leading_value) + " Link value:" + str(link_1.link_value) + " Leading value:" + str(link_1.trailing_address))
+
