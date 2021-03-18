7fb6a2bad1d3a7b6b84819ff084890b554da548f
SentryDev <FlamencoDeveloper@gmail.com>
2021-03-18 02:23:56 +0000

    Encapsulation of BellmandFord data structures and operations

--- BellmanFord.py
+++ BellmanFord.py
@@ -10,6 +10,9 @@
 # We define the graphical links using a class Data structure
 # Attach the leading node value, Link value and Trailing node address as Attributes
 
+# We store the value of the leading node and not the address, this allwos us to 
+# Reduce the additional access required in the Node value data structure
+
 class Link:
     
     def __init__(self, leading_value, link_value, trailing_address):
@@ -18,9 +21,30 @@ class Link:
         self.link_value = link_value
         self.trailing_address = trailing_address
 
-    
+#Encapsulation of data structures and operation related to Bellmand Ford
+#Allows decoupling from main execution code
+class BellmandFord:
 
+    def __init__(self, link_list):
+            self.graph_structure = link_list
 
+    def loop_graph(self):
+        for link in self.graph_structure:
+            print("Link value: " + str(link.link_value))
+
+# Manual instantiation of Links, however can be populated with API call
+# Followed by destructuring with loops
+# Instantiation of links + generation of list of Links data structure
+# Structuring of data from API can also be achieved using class encapsulation
+# This separates the computational data and the operations on the data
 link_1 = Link(6, 10, 2)
-print("Leading value:" + str(link_1.leading_value) + " Link value:" + str(link_1.link_value) + " Leading value:" + str(link_1.trailing_address))
+link_2 = Link(4, 5, 3)
+link_3 = Link(7, 7, 4)
+
+# Instantiation of links + list formation defines the graphical structure
+list_link_from_API = [link_1, link_2, link_3]
+
+#Create an instance of a Bellmand Ford operation
+BellmandFordOperation = BellmandFord(list_link_from_API)
+BellmandFordOperation.loop_graph()
 
